8d33dc6d2282c77076d645d5709b826b1ceef4b5
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2022-05-02 21:27:09 +0200

    Create messenger.js
    - Initial implementation of messenger

---
+++ sockets/messenger.js
@@ -0,0 +1,28 @@
+const {addMessage} = require("../repositories/messages");
+
+function storeUserSocket(io, userId, socket){
+    let listOfUserSockets;
+    listOfUserSockets = io.sockets.sockets.get(userId) || [];
+    listOfUserSockets.push(socket.id);
+    io.sockets.sockets.set(userId, listOfUserSockets);
+}
+
+function discardUserSocket(io, userId, socket){
+    let listOfSockets = io.sockets.sockets.get(userId);
+    listOfSockets = listOfSockets.filter(toBeFilteredSocket => socket.id === toBeFilteredSocket);
+    io.sockets.sockets.set(userId, listOfSockets);
+}
+
+async function sendMessage(io, senderId, receiverId, data){
+    console.log('sendMessage: ' +  data.msg.message)
+    await addMessage(data.conversationId, data.msg)
+    let receiverSockets = io.sockets.sockets.get(receiverId);
+    let response = {senderId: senderId, msg: data.msg.message.text};
+    io.to(receiverSockets).emit('receiveMessage', response);
+}
+
+module.exports = {
+    storeUserSocket,
+    discardUserSocket,
+    sendMessage
+}

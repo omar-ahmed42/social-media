e73f779c495ccf2e0f23c76d9fa424738f543810
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2022-06-28 23:26:13 +0200

    Delete memoryCache.js

--- cache/memoryCache.js
+++
@@ -1,7 +0,0 @@
-var memoryCache = module.exports = function () {
-    var cache = {};
-    return {
-        get: function (key) { return cache[key]; },
-        set: function (key, val) { cache[key] = val; }
-    }
-}

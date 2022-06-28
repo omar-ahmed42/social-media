13c5e5b5a9635665b22914a3b514d3189714308e
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2022-06-28 23:35:39 +0200

    Update TypeDefs.js
    - Removed unnecessary parameters

--- schema/TypeDefs.js
+++ schema/TypeDefs.js
@@ -1,4 +1,4 @@
-const { gql } = require('apollo-server-express');
+const {gql} = require('apollo-server-express');
 
 const typeDefs = gql`
     scalar Upload
@@ -52,6 +52,7 @@ const typeDefs = gql`
         getPostById(id: ID!): Post
         getCommentById(commentId: ID!): Comment
         getAllBlockedUsers: [Person]
+        getNewsFeed(lastSeenPostId: Int, size: Int):[Post]
         
     }
     
@@ -60,20 +61,20 @@ const typeDefs = gql`
         addPerson(firstName: String!, lastName: String!, email: String!, password: String!, dateOfBirth: String!, role: [String]!): Boolean!
         deletePerson(id: ID!): Boolean
         addPost(content: String, media: [Upload]): Boolean
-        deletePost(userId: ID!, postId: ID!): Boolean
-        addComment(userId: ID!, postId: ID!, content: String, media: [Upload]): Boolean
+        deletePost(postId: ID!): Boolean
+        addComment(postId: ID!, content: String, media: [Upload]): Boolean
         deleteComment(id: ID!): Boolean
-        blockUser(userId: ID!, userToBeBlockedId: ID!): Boolean
-        unblockUser(userId: ID!, userToBeBlockedId: ID!): Boolean
-        deleteFriendship(userId: ID!, friendId: ID!): Boolean
-        declineFriendRequest(receiverId: ID!, senderId: ID!): Boolean
-        cancelFriendRequest(receiverId: ID!, senderId: ID!): Boolean
-        acceptFriendRequest(senderId: ID!, receiverId: ID!): Boolean
+        blockUser(userToBeBlockedId: ID!): Boolean
+        unblockUser(userToBeBlockedId: ID!): Boolean
+        deleteFriendship(friendId: ID!): Boolean
+        declineFriendRequest(senderId: ID!): Boolean
+        cancelFriendRequest(receiverId: ID!): Boolean
+        acceptFriendRequest(senderId: ID!): Boolean
         sendFriendRequest(senderId: ID!, receiverId: ID!): Boolean
-        reactToPost(userId: ID!, postId: ID!, reactionTypeId: ID!): Boolean
+        reactToPost(postId: ID!, reactionTypeId: ID!): Boolean
         removeReactionFromPost(postId: ID!, reactionId: ID!): Boolean
-        reactToComment(userId: ID!, commentId: ID!, reactionTypeId: ID!): Boolean
-        removeReactionFromComment(commentId: ID!, reactionId: ID!): Boolean
+        reactToComment(commentId: ID!, reactionTypeId: ID!): Boolean
+        removeReactionFromComment(reactionId: ID!): Boolean
         addReactionType(reactionTypeName: String!): Boolean!
     }
 `;

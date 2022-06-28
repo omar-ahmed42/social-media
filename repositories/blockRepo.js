413bb9b0c5b4f013b833d53933cf7ecc358e07f4
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2022-06-28 23:34:48 +0200

    Update blockRepo.js

--- repositories/blockRepo.js
+++ repositories/blockRepo.js
@@ -3,29 +3,29 @@ const driverSession = require('../db/connect.js').driverSession;
 async function findAllBlockedUsers(userId){
     await driverSession.run(
         `
-        MATCH (person:PERSON) WHERE ID(person) = ${userId}
+        MATCH (person:PERSON) WHERE ID(person) = $userId
         (person) -[:BLOCKS]-> (blockedPerson:PERSON)
         RETURN blockedPerson
-        `
+        `, {userId: userId}
     );
 }
 
 async function blockUser(userId, blockedUserId){
     await driverSession.run(
         `
-        MATCH (user:PERSON) WHERE ID(user) = ${userId}
-        MATCH (blockedUser:PERSON) WHERE ID(blockedUser) = ${blockedUserId}
-        CREATE (user) -[:BLOCKS]-> (blockedUser)`
+        MATCH (user:PERSON) WHERE ID(user) = $userId
+        MATCH (blockedUser:PERSON) WHERE ID(blockedUser) = $blockedUserId
+        CREATE (user) -[:BLOCKS]-> (blockedUser)`, {userId: userId, blockedUserId: blockedUserId}
     );
 }
 
 async function unblockUser(userId, blockedUserId){
     await driverSession.run(`
-    MATCH (user:PERSON) WHERE ID(user) = ${userId}
-    MATCH (blockedUser:PERSON) WHERE ID(blockedUser) = ${blockedUserId}
+    MATCH (user:PERSON) WHERE ID(user) = $userId
+    MATCH (blockedUser:PERSON) WHERE ID(blockedUser) = $blockedUserId
     MATCH (user) -[blocks:BLOCKS]-> (blockedUser)
     DELETE blocks
-    `);
+    `, {userId: userId, blockedUserId: blockedUserId});
 }
 
 module.exports = {

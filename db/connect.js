b473f10947b932bc96e42db7c5f61711334b9852
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2023-07-09 12:17:09 +0300

    Add a sequelize connection

--- db/connect.js
+++ db/connect.js
@@ -2,13 +2,30 @@ var neo4j = require('neo4j-driver');
 var cassandra = require('cassandra-driver')
 var mysql = require('mysql2');
 const util = require('util');
+require('dotenv').config();
 
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    dialect: 'mysql'
+});
+
+const connectSequelize = async () => {
+    try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
 
 var driver = neo4j.driver('neo4j://localhost',
     neo4j.auth.basic('neo4j', 'root'));
-var driverSession = driver.session();
 
-require('dotenv').config();
+var driverSession = driver.session({database: 'neo4j'});
+
 const cassandraClient = new cassandra.Client({
     contactPoints: [`${process.env.CASSANDRA_HOST}`],
     localDataCenter: 'datacenter1',
@@ -36,9 +53,9 @@ const connectMySQL = async () => await mysqlConnection.connect(function(err) {
     console.log("MySQL Connected");
 });
 
-
 async function startDBs(){
     await connectCassandra();
+    await connectSequelize();
     await connectMySQL();
 
 }
@@ -46,6 +63,7 @@ async function startDBs(){
 const mysqlQuery = util.promisify(mysqlConnection.query).bind(mysqlConnection);
 
 module.exports = {
+    sequelize,
     driverSession,
     cassandraClient,
     mysqlConnection,

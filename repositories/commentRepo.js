b3e8cc75a908b8723f2c4fca04186a392423c366
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2023-07-17 23:10:46 +0300

    Modify comment service to make use of sequelize and separate attachment storage logic from comments
    Modify comment service to make use of sequelize and separate attachment storage logic from comments
    - Replace addComment with saveComment which is responsible for saving comments, validating their content and assigning a status to them such as draft/published
    - Add findCommentById and findCommentByPostId

--- repositories/commentRepo.js
+++ repositories/commentRepo.js
@@ -1,87 +1,171 @@
-const {cassandraClient, driverSession} = require("../db/connect");
-const fs = require("fs");
-const path = require("path");
-const {finished} = require("stream/promises");
-const {parseFileExtension} = require("../utils/parsers/file");
-const {isValidMedia} = require("../utils/validators/media");
-const {v4: uuidv4} = require("uuid");
+const { Comment, CommentStatusEnum } = require('../models/comment');
+const { Post, PostStatusEnum } = require('../models/post');
+const { isBlank } = require('../utils/string-utils');
 
-async function addComment(userId, msg) {
-    try {
-        let urls = await Promise.all((msg.media).map(async (file) => {
-            const {createReadStream, filename, mimetype, encoding} = await file;
+async function saveComment(userId, commentId, postId, msg) {
+  if (!postId) return null; // TODO: Throw an exception
+  let post = await Post.findByPk(postId);
+  if (!post) return null; // TODO: Throw an exception
+  if (post.getDataValue('postStatus') != PostStatusEnum.published) return null; // TODO: Throw an exception
 
-            const fileExtension = parseFileExtension(filename);
-            if (!isValidMedia(fileExtension)) {
-                // TODO: Throw error
-                throw new Error('Invalid media type');
-            }
+  let comment = null;
+  switch (msg.commentStatus) {
+    case CommentStatusEnum.draft:
+      comment = await saveCommentAsDraft(userId, commentId, postId, msg);
+      break;
+    case CommentStatusEnum.published:
+      comment = await publishComment(userId, commentId, postId, msg);
+      break;
+  }
 
-            const newFilename = uuidv4() + '_' + Date.now() + fileExtension; // Generation a unique filename
+  return comment.get();
+}
 
-            const stream = createReadStream();
-            const out = fs.createWriteStream(path.join(__dirname, `/../FileUpload/Comments/${newFilename}`));
-            stream.pipe(out);
-            await finished(out);
-            return `http://localhost:3000/FileUpload/Comments/${newFilename}`
-        }));
+async function saveCommentAsDraft(userId, commentId, postId, msg) {
+  if (!commentId)
+    return await Comment.create({
+      postId: postId,
+      userId: userId,
+      content: msg.content,
+      commentStatus: CommentStatusEnum.draft,
+    });
 
-        if (!urls && msg.content.length === 0) {
-            // TODO: Throw Error
-            throw new Error('Empty comment')
-        }
+  let comment = await Comment.findByPk(commentId);
+  if (!comment) return null;
+  if (comment.getDataValue('postId') !== postId) return null; // TODO: Throw an exception
+  if (comment.getDataValue('commentStatus') !== CommentStatusEnum.draft)
+    return null; // TODO: throw an exception
 
-        await driverSession.run(
-            `
-            MATCH (person:PERSON) WHERE ID(person) = $userId
-            MATCH (post:POST) WHERE ID(post) = $postId
-            CREATE (person) -[writes_comment:WRITES_COMMENT]-> (comment:COMMENT {content: $content, media: $media, creationDate: timestamp()})
-            CREATE (comment) -[comments_on:COMMENTS_ON]-> (post)`,
-            {userId: userId, content: msg.content, media: urls, postId: msg.postId}
-        )
-        return true;
-    } catch (e) {
-        console.error('ERROR: ' + e);
-        console.error('ERROR_CODE: ' + e.code);
-        return false;
+  comment.content = msg.content;
+  return await comment.save();
+}
+
+async function publishComment(userId, commentId, postId, msg) {
+  if (!commentId) {
+    if (!isBlank(msg.content))
+      return await Comment.create({
+        content: msg.content,
+        userId: userId,
+        postId: postId,
+        commentStatus: CommentStatusEnum.published,
+      });
+
+    return null; // TODO: Throw an exception
+  } else {
+    if (!isBlank(msg.content)) {
+      const [affectRowCount] = await Comment.update(
+        { content: msg.content, commentStatus: CommentStatusEnum.published },
+        { where: { id: commentId, userId: userId, postId: postId } }
+      );
+      return affectRowCount > 0 ? await Comment.findByPk(commentId) : null;
     }
+
+    let comment = await Comment.findByPk(commentId, {
+      include: [{ model: CommentAttachment, include: Attachment }],
+    });
+
+    if (!comment) return null;
+    if (comment.getDataValue('userId') !== userId) return null; // TODO: Throw an exception
+    if (comment.getDataValue('postId') !== postId) return null; // TODO: Throw an exception
+    if (comment.getDataValue('CommentAttachments')?.length == 0) return null; // TODO: Throw an exception
+
+    comment.set({
+      content: msg.content,
+      commentStatus: CommentStatusEnum.published,
+    });
+    const updatedComment = await comment.save();
+    saveCommentModelToCache(updatedComment);
+    return updatedComment;
+  }
+}
+
+async function saveCommentModelToCache(comment) {
+  try {
+    return await commentCacheRepository.save(
+      comment.getDataValue('id').toString(),
+      await transformToCacheableComment(
+        comment.get(),
+        comment.get().CommentAttachments
+      )
+    );
+  } catch (err) {
+    console.err(
+      'An error has occurred while saving comment model to cache:',
+      err
+    );
+  }
+}
+
+async function transformToCacheableComment(comment, commentAttachments) {
+  let cacheableComment = comment;
+
+  if (!commentAttachments?.length) return cacheableComment;
+
+  let attachmentURLs = [];
+  for (const commentAttachment of commentAttachments) {
+    const url = commentAttachment.Attachment.getDataValue('url');
+    attachmentURLs.push(url);
+  }
+
+  delete cacheableComment.PostAttachments;
+  cacheableComment.commentAttachments = attachmentURLs;
+  return cacheableComment;
 }
 
 async function deleteComment(userId, commentId) {
-    try {
-        await driverSession.run(
-            `
-            MATCH (person: PERSON) WHERE ID(person) = $userId
-            MATCH (person) -[writes_comment:WRITES_COMMENT]-> (comment:COMMENT) WHERE ID(comment) = $commentId
-            MATCH (comment) -[comments_on:COMMENTS_ON]-> (post:POST)
-            DETACH DELETE writes_comment
-            DETACH DELETE (comment)
-            DETACH DELETE comments_on
-            `, {userId: userId, commentId: commentId}
-        )
-        return true;
-    } catch (e) {
-        console.error('ERROR: ' + e);
-        console.error('ERROR_CODE: ' + e.code);
-        return false;
-    }
+  try {
+    return await Comment.destroy({
+      where: { id: commentId, userId: userId },
+    });
+  } catch (e) {
+    console.error('ERROR: ' + e);
+  }
 }
 
-async function findCommentById(commentId) {
-    try {
-        let res = await driverSession.run(`
-        MATCH (comment:COMMENT) WHERE ID(comment) = $commentId
-        RETURN (comment)
-    `, {commentId: commentId});
-        return res.records.map(record => record._fields[0].properties);
-    } catch (e) {
-        console.error('ERROR: ' + e);
-        console.error('ERROR_CODE: ' + e.code);
+async function findCommentById(commentId, userId) {
+  try {
+    let comment = await Comment.findByPk(commentId);
+    if (!comment) return null;
+    if (
+      comment.getDataValue('commentStatus') == CommentStatusEnum.draft &&
+      userId !== comment.getDataValue('userId')
+    ) {
+      return null; // TODO: Throw an exception
     }
+    return comment.get();
+  } catch (e) {
+    console.error('ERROR: ' + e);
+  }
+}
+
+async function findCommentsByPostId(postId) {
+  try {
+    let comments = await Comment.findAll({ where: { postId: postId } });
+    return comments.map((comment) => comment.get());
+  } catch (e) {
+    console.error('ERROR:', e);
+  }
+}
+
+async function findCommentByCommentAttachmentId(commentAttachmentId) {
+  try {
+    let commentAttachment = await PostAttachment.findByPk(commentAttachmentId, {
+      include: {
+        model: Comment,
+        attributes: ['id', 'content', 'createdAt', 'lastModifiedAt'],
+      },
+      attributes: [],
+    });
+    return commentAttachment ? commentAttachment.get().Comment : null;
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 module.exports = {
-    addComment,
-    deleteComment,
-    findCommentById
-}
+  deleteComment,
+  findCommentById,
+  saveComment,
+  findCommentsByPostId,
+  findCommentByCommentAttachmentId,
+};

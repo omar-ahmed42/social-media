d3219dbb32b6d8d0dc2782234a6ed8d8f1854025
Omar Ahmed Hassan <omar.ahmed.h2000@gmail.com>
2023-07-17 22:20:27 +0300

    Add a fanout service to handle newsfeed
    - Add a fanout service to handle the newsfeed in the following ways:
     1. By pushing published posts to the newsfeed of each user's friends
     2. By fetching the newsfeed for each user by retrieving posts ids from the cache then retrieve posts from the database

--- repositories/fanout.js
+++ repositories/fanout.js
@@ -1,6 +1,51 @@
-async function pushToNewsFeed(userId, post) {  
+const { Op } = require('sequelize');
+const { redisClient } = require('../cache/client');
+const { Post } = require('../models/post');
+const { findAllFriends } = require('./friendRepo');
+const { savePostModelToCache } = require('./postRepo');
+
+const MAX_NEWSFEED_SIZE = 150;
+
+async function pushToNewsFeed(userId, postId) {
+  try {
+    let friends = await findAllFriends(userId);
+
+    if (!friends?.length) return 0;
+
+    for (let friend of friends) {
+      redisClient
+        .multi()
+        .LPUSH(`newsfeed:${friend.id.toString()}`, postId.toString())
+        .LTRIM(`newsfeed:${friend.id.toString()}`, 0, MAX_NEWSFEED_SIZE)
+        .exec();
+    }
+  } catch (err) {
+    console.err('An error has occurred during pushing to newsfeed:', err);
+  }
+}
+
+async function fetchNewsfeed(userId) {
+  let postsIds = await redisClient.LRANGE(`newsfeed:${userId.toString()}`, 0, -1);
+  let posts = [];
+  for (let postId of postsIds) {
+    posts.push(await getAndSetPost(userId, postId));
+  };
+
+  return posts;
+}
+
+async function getAndSetPost(userId, postId) {
+  let post = await redisClient.json.GET(`post:${postId.toString()}`);
+  if (post) return post;
+
+  post = await Post.findByPk(postId, {
+    include: [{ model: PostAttachment, include: Attachment }],
+  });
+
+  return await savePostModelToCache(post);
 }
 
 module.exports = {
   pushToNewsFeed,
+  fetchNewsfeed,
 };
